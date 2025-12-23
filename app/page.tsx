cfb64dad4d7ae0e9c27f269b412e3bbd217f17b7
unknown <alan.gabriel.luna@hotmail.com>
2025-12-23 18:43:55 -0300

    promocion lavado

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <HeroSection />
-      <ServicesSection />
       <PromotionSection />
+      <ServicesSection />
       <GallerySection />
       <BrandsSection />
       <ContactSection />
